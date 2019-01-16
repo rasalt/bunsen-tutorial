5e291f0792b696dc1d8123dc2f2eb13e328d94a4
Dharmesh Patel <patel.dharmesh@gmail.com>
2019-01-16 13:52:46 -0800

    Update env.sh

--- scripts/provisioning/env.sh
+++ scripts/provisioning/env.sh
@@ -1,6 +1,6 @@
-export PROJECT="<<GCP Project ID"
-export REGION="GCP Region"
-export ZONE="GCP Zone"
-export clustername="Cluster Name"
+export PROJECT="<<GCP Project ID>>"
+export REGION="<<GCP Region>>"
+export ZONE="<<GCP Zone>>"
+export clustername="<<Cluster Name>>"
 export bucketname=$clustername"-bkt"
 gsutil mb gs://$bucketname
