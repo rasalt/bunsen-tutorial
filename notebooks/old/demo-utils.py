b153ffbf5471f102e0cb7500703a6153c76b99ef
Dharmesh Patel <patel.dharmesh@gmail.com>
2019-01-21 15:18:34 -0800

    putting py in notebooks

---
+++ notebooks/old/demo-utils.py
@@ -0,0 +1,12 @@
+def age(birthdate):
+    born = datetime.strptime(birthdate, "%Y-%m-%d")
+    today = date.today()
+    return str(today.year - born.year - ((today.month, today.day) < (born.month, born.day)))
+
+def los(row):
+    #start = datetime.strptime(row['start'], '%Y-%m-%dT%H:%M:%S%z')
+    start = parse(row['start'])
+    end = parse(row['end'])
+    #end = datetime.strptime(row['end'], '%Y-%m-%dT%H:%M:%S%z')
+    los = end-start
+    return str(los)
