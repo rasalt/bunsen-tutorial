dd88b3f0f68a94dc39f65d008f47b60227be6218
Dharmesh Patel <patel.dharmesh@gmail.com>
2019-01-21 14:50:36 -0800

    Added utils.py

--- scripts/provisioning/01-prep.sh
+++ scripts/provisioning/01-prep.sh
@@ -2,4 +2,4 @@
 #Reference link
 
 gsutil cp ../datalab/* gs://$bucketname/scripts/
-gcloud sql instances create $clustername --database-version="MYSQL_5_7" --activation-policy=ALWAYS --gce-zone $ZONE
+gcloud sql instances create $dbname --database-version="MYSQL_5_7" --activation-policy=ALWAYS --zone $ZONE
