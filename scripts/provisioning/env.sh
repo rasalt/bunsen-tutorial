7841953b0150b1264299997ef33266cde23178bd
Dharmesh Patel <patel.dharmesh@gmail.com>
2019-01-23 14:05:29 -0800

    updated env.sh

--- scripts/provisioning/env.sh
+++ scripts/provisioning/env.sh
@@ -3,4 +3,7 @@ export REGION="<<GCP Region>>"
 export ZONE="<<GCP Zone>>"
 export clustername="<<Cluster Name>>"
 export bucketname=$clustername"-bkt"
+export dbname=$clustername"-db"
+echo "Using "$clustername " with follwing bucket:"
+gsutil ls gs://$bucketname ||
 gsutil mb gs://$bucketname
