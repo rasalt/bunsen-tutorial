b70ce64d078480b2210c2876ad3d9c682125719a
Dharmesh Patel <patel.dharmesh@gmail.com>
2019-01-23 14:17:11 -0800

    Spelling correction

--- scripts/provisioning/env.sh
+++ scripts/provisioning/env.sh
@@ -4,6 +4,6 @@ export ZONE="<<GCP Zone>>"
 export clustername="<<Cluster Name>>"
 export bucketname=$clustername"-bkt"
 export dbname=$clustername"-db"
-echo "Using "$clustername " with follwing bucket:"
+echo "Using "$clustername " with following bucket:"
 gsutil ls gs://$bucketname ||
 gsutil mb gs://$bucketname
