02ac40e45df18c0eb511f8ba1c3d32c216ad3880
Ruchika k <ruchika.kharwar@gmail.com>
2019-01-25 14:02:23 -0600

    Hive Metastore hooks: Exapnsion of scripts to use an SQL hive metastore
    Use the hivemeta flags with the preparatory and cluster provisioning scripts
    to use a persistent hive metastore
    
    Signed-off-by: Ruchika k <ruchika.kharwar@gmail.com>

--- scripts/provisioning/01-prep.sh
+++ scripts/provisioning/01-prep.sh
@@ -1,5 +1,23 @@
+if [[ $# -eq 0 ]] ; then
+    echo "This deployment will not use an Hive metastore"
+    echo "If you do want to use an a hive metastore with this deployment please pass in argument 'hivemeta'"
+fi
+
 #Create persistent hive metadata store
+
 #Reference link
+gsutil mb gs://$bucketname
 
 gsutil cp ../datalab/* gs://$bucketname/scripts/
-gcloud sql instances create $dbname --database-version="MYSQL_5_7" --activation-policy=ALWAYS --zone $ZONE
+gsutil ls gs://$bucketname/scripts/
+
+#Create the sql database if you want to leverage an sql hive metasstore 
+if [[ "$1" == "hivemeta" ]] ; then
+    gsutil ls gs://$hivebucketname
+    if [ $? -eq 0 ]; then
+      echo "Bucket $hivebucketname exists and the dataproc cluster will leverage this existing bucket"
+    else
+      gsutil mb gs://$hivebucketname
+    fi
+    gcloud sql instances create $hivedbname --database-version="MYSQL_5_7" --activation-policy=ALWAYS --gce-zone $ZONE
+fi
