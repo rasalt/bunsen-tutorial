1bb039c6d097fcf7f15390e40990e7e9a4eec6a1
Ruchika k <ruchika.kharwar@gmail.com>
2019-01-25 14:41:36 -0600

    Minor fix for variable names

--- scripts/provisioning/env.sh
+++ scripts/provisioning/env.sh
@@ -5,8 +5,8 @@ export clustername="<<Cluster Name>>"
 export bucketname=$clustername"-bkt"
 
 #Sql hive metastore information .. optional 
-export hivebucketname="Unique sqlhive bucketname" #Please note: this bucket is created once
-export hivedbname="<Set the database name>"
+export hivebucketname="" #Unique sqlhive bucketname please note: this bucket is created once
+export hivedbname="" #Set the database name
 #Information output
 echo "Using "$clustername " with the following bucket $bucketname"
 echo "Using "$clustername " with the following bucket sql hive bucketname $hivebucketname"
